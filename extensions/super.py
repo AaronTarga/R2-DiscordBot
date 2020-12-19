08e4a37e014d93756bfe7974300d650add2c0a86
Aaron Targa <33754619+AaronTarga@users.noreply.github.com>
2020-12-19 17:26:21 +0100

    fix: fixed clean function and tests with reactions

--- extensions/super.py
+++ extensions/super.py
@@ -14,8 +14,17 @@ class Super(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def clean(self,ctx,amount=5):
         "Cleans given amount of messages(only for people who can delete messages)"
+        if amount < 1:
+            return await ctx.send(embed=discord.Embed(description="Amount of deleted messages must be greater than zero!", color=settings.error_color))
+        amount = amount + 1 #+1 to delete clean message itselft too
         deleted = await ctx.channel.purge(limit=amount)
         await ctx.send(embed = discord.Embed(description='**Deleted {} message(s)**'.format(len(deleted)),color=settings.color))
+
+@client.event
+async def on_reaction_add(reaction,user):
+    channel = reaction.message.channel
+    await ctx.send_message(channel, '{} has reacted with {} to the message: {}'.format(user.name,reaction.emoji,reaction.message.content))
+
 
 def setup(client):
     client.add_cog(Super(client))
